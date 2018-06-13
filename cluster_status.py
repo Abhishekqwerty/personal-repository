d36149e8c23a2050a248e2dcd9524ffc71ba068f
Abhishekqwerty <gaikwadabhishek978@gmail.com>
2018-06-13 08:37:52 +0530

    Update cluster_status.py

--- cluster_status.py
+++ cluster_status.py
@@ -141,5 +141,4 @@ credentials = sys.argv
 hostname = credentials[2]
 username = credentials[4]
 password = credentials[6]
-print(hostname + " " + username + " " + password)
 status(hostname, username, password)
